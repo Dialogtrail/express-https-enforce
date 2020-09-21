ed9fcef60afac9e4c8069a85d1a0de43ec38e141
mightynerd <staszw@gmail.com>
2020-09-21 10:48:12 +0200

    Add check of x-forwarded-proto header

--- src/enforceHttps.ts
+++ src/enforceHttps.ts
@@ -9,7 +9,7 @@ const enforceHttps = (whitelist: string[] = []) => (req, res, next) => {
 };
 
 const checkRequest = (whitelist: string[], req) => {
-  if (req.secure) {
+  if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
     return true;
   } else if (whitelist.includes(process.env.NODE_ENV)) {
     return true;
